5b72be100fba3971dde7d5184513c2b2760ed4ac
Gormi3815 <gormi3815@wrdsb.ca>
2020-10-06 13:38:05 +0000

    this program will tell you if the number you entered is a prime number

--- Main.java
+++ Main.java
@@ -1,15 +1,33 @@
+import java.util.Scanner;
+
 /**
+ * this program will tell you if the number inputed is a prime number
  *
- * @author 
+ * @author isaac
  */
 public class Main {
 
   /**
    * The method that is executed when you hit the run button.
+   * 
    * @param args the command line arguments
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    System.out.println("What number do you want to check?");
+    int number = input.nextInt();
+    int counter =2;
+    while(counter < number){
+      if(number % counter == 0){
+        System.out.println(number + " is not a prime number.");
+
+      }
+      counter = counter + 1;
+    }
+    if(number == counter){
+      System.out.println(number + " is not a prime number.");
+    }
     
   }
 }
